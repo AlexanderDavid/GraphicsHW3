f03c41ec48e6d1c724114fe7254ef9e9246b39b5
Alex Day <alexday135@gmail.com>
2021-09-01 16:16:40 -0400

    Make Viewer into an ABC

--- include/Viewer.hh
+++ include/Viewer.hh
@@ -8,20 +8,59 @@
 
 namespace viewer
 {
+    /**
+     * Abstract base class for defining OpenGL applications
+     **/
     class Viewer
     {
     public:
-        static auto setup(int* argc, char** argv) -> void;
+        /**
+         * Construct the singleton instance of the viewer.
+         * TODO: Make into an actual singleton
+         **/
+        Viewer();
 
-        static auto setDisplayCallback(void (*fn)()) -> void;
-        static auto setReshapeCallback(void (*fn)(int, int)) -> void;
-        static auto setMotionCallback(void (*fn)(int, int)) -> void;
-        static auto setMouseCallback(void (*fn)(int, int, int, int)) -> void;
+        /**
+         * Set up the OpenGL window and canvas
+         *
+         * @param argc Number of command line arguments
+         * @param argv Command line arguments
+         * @param width Width of the screen, Optional, default 500
+         * @param height Height of the screen, Optional, default 500
+         **/
+        static auto setup(int* argc, char** argv, int width = 500, int height = 500) -> void;
 
-        static auto go() { glutMainLoop(); }
+
+        /**
+         * Start the OpenGL main loop. Blocking.
+         **/
+        static auto go() -> void { glutMainLoop(); }
+
+    protected:
+        /**
+         * Class level openGL callbacks. These contain the actual callback code that will be called
+         * by the static methods
+         **/
+        virtual auto display() -> void                                 = 0;
+        virtual auto resize(int width, int height) -> void             = 0;
+        virtual auto motion(int x, int y) -> void                      = 0;
+        virtual auto mouse(int b, int state, int x, int y) -> void     = 0;
+        virtual auto keyboard(unsigned char key, int x, int y) -> void = 0;
+
+
+        static Viewer* instance_;
 
     private:
-        static std::once_flag flag_;
+        /**
+         * Static method callbacks using the singleton of the viewer for OpenGL
+         **/
+        static auto displayCb() -> void { instance_->display(); }
+        static auto resizeCb(int width, int height) -> void { instance_->resize(width, height); }
+        static auto motionCb(int x, int y) -> void { instance_->motion(x, y); }
+        static auto mouseCb(int b, int state, int x, int y) -> void
+        {
+            instance_->mouse(b, state, x, y);
+        }
     };
 }
 
--- src/Viewer.cc
+++ src/Viewer.cc
@@ -6,20 +6,27 @@
 
 namespace viewer
 {
-    auto Viewer::setup(int* argc, char** argv) -> void
+    Viewer::Viewer()
+    {
+        // Set the instance for the static opengl methods :(
+        instance_ = this;
+    }
+
+    auto Viewer::setup(int* argc, char** argv, int width, int height) -> void
     {
         glutInit(argc, argv);
         glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-        glutInitWindowSize(500, 500);
+        glutInitWindowSize(width, height);
         glutInitWindowPosition(100, 100);
-        glutCreateWindow("OpenGL Program");
+        glutCreateWindow("CPSC 6050");
 
         glClearColor(0.0, 0.0, 0.0, 1.0);
         glEnable(GL_DEPTH_TEST);
+
+        glutDisplayFunc(&displayCb);
+        glutReshapeFunc(&resizeCb);
+        glutMotionFunc(&motionCb);
+        glutMouseFunc(&mouseCb);
     }
 
-    auto Viewer::setDisplayCallback(void (*fn)()) -> void { glutDisplayFunc(fn); }
-    auto Viewer::setReshapeCallback(void (*fn)(int, int)) -> void { glutReshapeFunc(fn); }
-    auto Viewer::setMotionCallback(void (*fn)(int, int)) -> void { glutMotionFunc(fn); };
-    auto Viewer::setMouseCallback(void (*fn)(int, int, int, int)) -> void { glutMouseFunc(fn); }
 }
