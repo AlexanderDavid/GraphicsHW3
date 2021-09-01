22b05bfba5d3de2470e4beaedd2a0cd058c66a2c
Alex Day <alexday135@gmail.com>
2021-09-01 15:06:30 -0400

    Remove triangle generation code

--- src/main.cc
+++ src/main.cc
@@ -9,16 +9,8 @@
 
 auto main(int argc, char** argv) -> int
 {
-    auto tv = triangleviewer::TriangleViewer(argc, argv);
-
     srand48(209238434234);
-
-    auto triangles = triangle::Triangle::generateTriangles(30, 0.523599);
-
-    for (int i = 0; i < triangles.size(); i++)
-    {
-        tv.add(triangles[i]);
-    }
+    auto tv = triangleviewer::TriangleViewer(argc, argv, 30, 0.523599);
 
     tv.go();
 
