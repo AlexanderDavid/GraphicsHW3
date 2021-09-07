84c06bb870192f290327a7e384c9ada3b15b9fb7
Alex Day <alexday135@gmail.com>
2021-09-06 21:53:02 -0400

    Clean up main

--- src/main.cc
+++ src/main.cc
@@ -3,16 +3,22 @@
 #include "TriangleViewer.hh"
 #include "Triangle.hh"
 
-#include <stdlib.h>
+#include <cstdlib>
 #include <iostream>
 #include <memory>
 
 
 auto main(int argc, char** argv) -> int
 {
-    srand48(209238434234);
-    auto tv = viewer::TriangleViewer(argc, argv, 30, 0.523599);
+    // Initialize the prng
+    srand48(19581623);
 
+    // Variables from the project handout
+    constexpr auto numTriangles = 30;
+    constexpr auto maxAngle     = 30 * M_PI / 180;
+
+    // Initialize and start (blocking) the triangle viewer
+    auto tv = viewer::TriangleViewer(argc, argv, numTriangles, maxAngle);
     tv.go();
 
     return 0;
